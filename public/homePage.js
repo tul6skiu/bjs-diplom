158c69ab65b9e4221d3f9e9cf740df4b77cf9495
wlistertula <tulaa478@gmail.com>
2025-04-05 16:54:03 +0300

    добавил получения курса валют, пользователя

--- public/homePage.js
+++ public/homePage.js
@@ -1,4 +1,5 @@
-const logout = new LogoutButton()
+const logout = new LogoutButton();
+const ratesBoard = new RatesBoard();
 
 logout.action = this.action = () => {
     ApiConnector.logout(response => {
@@ -7,3 +8,31 @@ logout.action = this.action = () => {
       }
     });
   };
+
+
+//получение текущего пользователя и отображение информации в виджете профиля
+ApiConnector.current(response => {
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);
+    } else {
+        console.error('Ошибка получения данных:', response.error);
+    }
+});
+
+
+// запрос получения курсов валют.
+function getStocks() {
+    ApiConnector.getStocks(response => {
+        if (response.success) {
+            ratesBoard.clearTable();
+            ratesBoard.fillTable(response.data);
+        }else {
+            console.error('Ошибка получения данных:', response.error);
+        }
+    });
+}
+//делаем один вызов
+getStocks();
+
+//обновляем каждую минуту
+const updateCurrency =  setInterval(() => getStocks(), 10000);
